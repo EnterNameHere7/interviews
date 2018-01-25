d109baf6132b48bd571025b3b104d6341f43914c
EnterNameHere7 <rudolfnel111@gmail.com>
2018-01-25 17:08:46 +0200

    Updates V2
    Shorten the code and also added comments

--- php/ProductController.php
+++ php/ProductController.php
@@ -18,9 +18,7 @@ class ProductController {
      * @param $name
      * @return
      */
-
     public function getById_GET(Application $app, $id){
-        //Initialize array and set data to request by
         $requestData = array();
         $requestData['id'] = $id;
 
@@ -29,35 +27,21 @@ class ProductController {
     }
 
     function  getInfo($requestData){
-        //Initialize curl
         $curl = curl_init();
-
-        //Set curl options and execute
         curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POST, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($curl);
-
-        //Decode Json response
         $response = json_decode($response);
-        //Close connection
         curl_close($curl);
         $result = array();
-
-        //Loop through the response
-        for ($i = 0; $i <= count($response); $i++) {
-
-            //Initialize array and load values into it
+        for ($i = 0; $i < count($response); $i++) {
             $prod = array();
             $prod['ean'] = $response[$i]['barcode'];
             $prod["name"]= $response[$i]['itemName'];
             $prod["prices"] = array();
-
-            //Loop through all the prices
-            for ($j=0 ; $j <= count($response[$i]['prices']); $j++) {
-
-                //Check if the currency is not ZAR
+            for ($j=0;$j < count($response[$i]['prices']); $j++) {
                 if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
                     $p_price = array();
                     $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
@@ -65,10 +49,8 @@ class ProductController {
                     $prod["prices"][] = $p_price;
                 }
             }
-            //Push array values into result array
             $result[] = $prod;
         }
-        //Return result
         return $result;
     }
 
@@ -81,11 +63,12 @@ class ProductController {
      * @return
      */
     public function getByName_GET(Application $app, $name){
-        //Initialize array and set data to request by
+
         $requestData = array();
         $requestData['names'] = $name;
 
         $result[] = $this->getInfo($requestData);
+
         return $app->render('products/products.twig', $result);
     }
 
