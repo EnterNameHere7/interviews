24aa62228786dcdc6c1699abdce1bc381bfef0f0
EnterNameHere7 <rudolfnel111@gmail.com>
2018-01-25 17:05:39 +0200

    Updates

--- php/ProductController.php
+++ php/ProductController.php
@@ -18,35 +18,58 @@ class ProductController {
      * @param $name
      * @return
      */
+
     public function getById_GET(Application $app, $id){
-        $curl = curl_init();
+        //Initialize array and set data to request by
         $requestData = array();
         $requestData['id'] = $id;
+
+        $result[] = $this->getInfo($requestData);
+        return $app->render('products/product.detail.twig', $result);
+    }
+
+    function  getInfo($requestData){
+        //Initialize curl
+        $curl = curl_init();
+
+        //Set curl options and execute
         curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($curl);
+
+        //Decode Json response
         $response = json_decode($response);
+        //Close connection
         curl_close($curl);
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
+        $result = array();
+
+        //Loop through the response
+        for ($i = 0; $i <= count($response); $i++) {
+
+            //Initialize array and load values into it
             $prod = array();
-            $prod['ean'] =$response[$i]['barcode'];
-            $prod["name"]=$response[$i]['itemName'];
+            $prod['ean'] = $response[$i]['barcode'];
+            $prod["name"]= $response[$i]['itemName'];
             $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
+
+            //Loop through all the prices
+            for ($j=0 ; $j <= count($response[$i]['prices']); $j++) {
+
+                //Check if the currency is not ZAR
                 if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
                     $p_price = array();
                     $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['curreny'] = $response[$i]['prices'][$j]['currencyCode'];
+                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
                     $prod["prices"][] = $p_price;
                 }
             }
+            //Push array values into result array
             $result[] = $prod;
         }
-
-        return $app->render('products/product.detail.twig', $result);
+        //Return result
+        return $result;
     }
 
     /**
@@ -58,33 +81,11 @@ class ProductController {
      * @return
      */
     public function getByName_GET(Application $app, $name){
-        $curl = curl_init();
+        //Initialize array and set data to request by
         $requestData = array();
         $requestData['names'] = $name;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] = $response[$i]['barcode'];
-            $prod["name"]= $response[$i]['itemName'];
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }
 
+        $result[] = $this->getInfo($requestData);
         return $app->render('products/products.twig', $result);
     }
 
